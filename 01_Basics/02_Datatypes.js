218deeedd92da9922de9fdb3093354d1f035ed67
aayushmagar <131133475+aayushmagar@users.noreply.github.com>
2023-12-25 17:39:27 +0000

    Discussion of Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // It treat all JS code as newer version
+
+//alert(3+3)*/we are not using this because we are working in node js
+
+//code readability should be high
+
+let name = "Aayush"
+let age = 21
+let isLoggedIn = true
+
+//number= 2 to power 3 is equal to 8
+// bigint = it is used for bigger number like trading, sharemarket etc
+// boolean = true/false
+//null = standalone value
+//undefined =
+//symbol = unique
+
+// these all are primitive data
+console.log(typeof age);
+console.log(typeof symbol);
+console.log(typeof null);
+console.log(typeof boolean);
+
