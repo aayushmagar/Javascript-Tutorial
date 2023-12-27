2aea307180d6c5649f9065f0e2057fb995dc0f52
aayushmagar <131133475+aayushmagar@users.noreply.github.com>
2023-12-27 10:45:54 +0000

    Conversion of Operation

---
+++ 01_Basics/03_Conversion_Operation.js
@@ -0,0 +1,16 @@
+let score = null
+//console.log(typeof score);
+let valueInNumber = Boolean(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" = 33
+//"33abc"=NaN
+//true = 1; False=0
+//"" = false
+//"aayush"=true
+
+
+let someNumber = "42"
+let stringNumber =String(someNumber)
+console.log(stringNumber);
